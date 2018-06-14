631e7d3d4e9b10b81d3b8adeb95bc0f51e429a66
famuyiwad <dare.famuyiwa@hmhco.com>
2018-06-14 18:37:46 +0100

    Added debugging message to AvroParquetWriter

--- src/main/java/com/pinterest/secor/io/impl/AvroParquetFileReaderWriterFactory.java
+++ src/main/java/com/pinterest/secor/io/impl/AvroParquetFileReaderWriterFactory.java
@@ -85,6 +85,12 @@ public class AvroParquetFileReaderWriterFactory implements FileReaderWriterFacto
     protected GenericRecord decodeMessage(byte[] value, String topic, SpecificDatumReader<GenericRecord> reader) throws IOException {
         // Avro schema registry header format is a "Magic Byte" that equals 0 followed by a 4-byte int
         // https://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format
+        String subjectOverride = getSchemaSubjectOverride(topic);
+        if (!subjectOverride.isEmpty()) {
+            topic = subjectOverride;
+        } else if (!schemaSubjectSuffix.isEmpty()) {
+            topic += schemaSubjectSuffix;
+        }
         if (value.length > 5 && value[0] == 0) {
             return schemaRegistryClient.decodeMessage(topic, value);
         } else {
